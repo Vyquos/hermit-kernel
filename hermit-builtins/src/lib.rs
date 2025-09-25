bb3be8103caff6adf416ba14f0d24a52e1f3ee0b
Vyquos <75266055+Vyquos@users.noreply.github.com>
2025-09-25 14:58:48 +0200

    Add fenv stubs for compatibility

--- hermit-builtins/src/lib.rs
+++ hermit-builtins/src/lib.rs
@@ -1,7 +1,21 @@
 #![no_std]
 #![feature(linkage)]
 
+use core::ffi::{c_int};
+
 pub mod math;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn fegetround() -> c_int {
+    // Rust doesn't support rounding modes. The rounding mode is not determinable.
+    -1
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn fesetround(_rounding_mode: c_int) -> c_int {
+    // Rust doesn't support rounding modes. Always fail (!= 0)
+    1
+}
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
